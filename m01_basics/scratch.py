ea60c7b9273c0c38a61ca0f7ad1ac9483d78282b
osamakhanedu <osamakhanedu@gmail.com>
2025-02-23 13:40:31 +0500

    add scratch.py with sample data and print functionality

---
+++ m01_basics/scratch.py
@@ -0,0 +1,20 @@
+name = "learning"
+names = ["osama","learning","python"]
+
+persons = [{
+    "name": "osama",
+    "location": "egypt",
+    "handsome": True,
+    "intelligent": True
+},
+{
+    "name": "ahmed",    
+    "location": "egypt",
+    "handsome": False,
+    "intelligent": False
+}
+
+]
+
+for person in persons:
+    print(f"my name is {person}")
