edb91afcf0599bb0d1894f4a8f86a9e70020ff52
osamakhanedu <osamakhanedu@gmail.com>
2025-03-02 13:32:57 +0500

    add 0_device.py with device dictionary and print functionality

---
+++ m01_basics/0_device.py
@@ -0,0 +1,27 @@
+from pprint import pprint
+
+# dictionary 
+
+device = {
+    "name": "sbx-n9",
+    "vendor": "cisco",
+    'model': "Modle", 
+    "os": "nxos",
+    "version": "9.3",
+    "ip": "10.1.11"
+}
+
+# print 
+print("=== Simple === ")
+print(device)
+print(device['model'])
+
+
+# pp 
+print("=== prety pring === ")
+pprint(device)
+
+# use loop to format 
+pprint("print using loop ")
+for key,value in device.items():
+    print(f"{key:>16s} : {value}")
